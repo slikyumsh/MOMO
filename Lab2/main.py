969fd910de0c236512c0521f6452b86ab23d282b
Sergey Tolmachev <mr.tolmse121@gmail.com>
2024-10-22 20:13:41 +0000

    fix bfgs

--- Lab2/main.py
+++ Lab2/main.py
@@ -7,80 +7,63 @@ def f(x):
 
 
 def grad(x):
-    return np.array([2 * x[0] - x[1] - 1, -x[0] + 2 * x[1] - x[2], 2 * x[2] - x[1]],dtype=np.float32)
+    return np.array([2 * x[0] - x[1] - 1, -x[0] + 2 * x[1] - x[2], 2 * x[2] - x[1]])
 
 
-def gesse_test():
-    return np.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]], dtype=np.float32)
-
-
-def inverse_gesse_test():
-    return np.array([[0.75, 0.5, 0.25], [0.5, 1, 0.5], [0.25, 0.5, 0.75]], dtype=np.float32)
-
-
-def gesse_initial():
-    return np.ones((3, 3), dtype=np.float32)
-
-
-def bfgs(x=np.array([0, 0, 0], dtype=np.float32), epsilon=0.5, c1=0.0001, c2=0.9, a_max=1, max_iter=10):
-    i = 0
-    s = 0
-    y = 0
-    H = gesse_initial()
-    norm = np.linalg.norm(grad(x))
-    while norm > epsilon:
-        print(H)
-        p = -H.dot(grad(x))
-        a = linear_step_size_search(f, grad, x, p, c1, c2, a_max, max_iter)
-        x = x + a * p
+def bfgs(x=np.array([0, 0, 0]), epsilon=1e-5, c1=0.0001, c2=0.9, a_max=1, max_iter=1000, search_max_iter=100):
+    I = np.eye(3)
+    H = I
+    for _ in range(max_iter):
+        g = grad(x)
+        if np.linalg.norm(g) < epsilon:
+            return x
+        p = -H @ g
+        a = linear_step_size_search(x, p, c1, c2, a_max, search_max_iter)
         s = a * p
-        y = grad(x) - grad(x - s)
+        new_x = x + s
+        new_g = grad(new_x)
+        y = new_g - g
 
-        precompute = y.transpose().dot(s)
-        precompute = 1 if precompute == 0 else precompute
+        # y.transpose() here has no effect ; (3,) -> (3,)
+        q = (1. / (y.transpose() @ s))
+        # Might be error. Exists variation where second np.outer must be (y,s) instead of what in formula in lab2
+        H = (I - q * np.outer(s, y)) @ H @ (I - q * np.outer(s, y)) + q * np.outer(s, s)
+        x = new_x
 
-        precompute = 1 / precompute
-        precompute = precompute * s
-
-        H = (np.ones((3, 3), dtype=np.float32) - precompute.dot(y.transpose()))\
-            .dot(H).dot(np.ones((3, 3), dtype=np.float32) - precompute.dot(y.transpose()))\
-            + precompute.dot(s.transpose())
-        i += 1
-        norm = np.linalg.norm(grad(x))
-        print(f"i={i} norm={norm} x={x}")
     return x
 
 
-def linear_step_size_search(f, grad, x, p, c1, c2, a_max, max_iter):
-    a = [0.01, (0.01 + a_max) / 2]
+def linear_step_size_search(x, p, c1, c2, a_max, max_iter):
+    a = [0, (0 + a_max) / 2]
     for i in range(1, max_iter):
         approx_f_value = f(x + a[i] * p)
-        if approx_f_value > f(x) + c1 * a[i] * grad(x).transpose().dot(p) or (approx_f_value >= f(x + a[i - 1] * p) and i > 1):
-            return zoom(f, grad, x, p, a[i - 1], a[i], c1, c2)
+        if approx_f_value > f(x) + c1 * a[i] * grad(x).transpose() @ p or (approx_f_value >= f(x + a[i - 1] * p) and i > 1):
+            return zoom(x, p, a[i - 1], a[i], c1, c2)
         approx_grad_value = grad(x + a[i] * p)
         if np.linalg.norm(approx_grad_value) <= -c2 * np.linalg.norm(grad(x)):
             return a[i]
-        if approx_grad_value.transpose().dot(p) >= 0:
-            return zoom(f, grad, x, p, a[i - 1], a[i], c1, c2)
+        if approx_grad_value.transpose() @ p >= 0:
+            return zoom(x, p, a[i - 1], a[i], c1, c2)
         a.append((a_max - a[i]) / 2 + a[i])
     return a[-1]
 
 
-def zoom(f, grad, x, p, a_low, a_high, c1, c2):
+def zoom(x, p, a_low, a_high, c1, c2):
     while True:
         a_j = (a_low + a_high) / 2
-        if f(x + a_j * p) > f(x) + c1 * a_j * grad(x).transpose().dot(p) or f(x + a_j * p) >= f(x + a_low * p):
+        if f(x + a_j * p) > f(x) + c1 * a_j * grad(x).transpose() @ p or f(x + a_j * p) >= f(x + a_low * p):
             a_high = a_j
         else:
             approx_grad = grad(x + a_j * p)
-            if np.linalg.norm(approx_grad) <= -c2 * grad(x).transpose().dot(p):
+            if np.linalg.norm(approx_grad) <= -c2 * grad(x).transpose() @ p:
                 return a_j
-            if approx_grad.transpose().dot(p) * (a_high - a_low) >= 0:
+            if approx_grad.transpose() @ p * (a_high - a_low) >= 0:
                 a_high = a_low
             a_low = a_j
 
-        if math.fabs(a_low - a_high) < 1e-4:
-            return a_low
+        if math.fabs(a_low - a_high) <= 1e-2:
+            return (a_low + a_high) / 2
+
 
 x = bfgs()
-print(x)
+print(f'Funtion min point = {x}; f(x) = {f(x)}')
