f756bbae9c5aac4e1646b0dcb6849125d0342482
Sergey Tolmachev <mr.tolmse121@gmail.com>
2024-10-22 21:09:55 +0000

    add l-bfgs

--- Lab2/main.py
+++ Lab2/main.py
@@ -13,10 +13,10 @@ def grad(x):
 def bfgs(x=np.array([0, 0, 0]), epsilon=1e-5, c1=0.0001, c2=0.9, a_max=1, max_iter=1000, search_max_iter=100):
     I = np.eye(3)
     H = I
-    for _ in range(max_iter):
+    for iter in range(max_iter):
         g = grad(x)
         if np.linalg.norm(g) < epsilon:
-            return x
+            break
         p = -H @ g
         a = linear_step_size_search(x, p, c1, c2, a_max, search_max_iter)
         s = a * p
@@ -30,7 +30,47 @@ def bfgs(x=np.array([0, 0, 0]), epsilon=1e-5, c1=0.0001, c2=0.9, a_max=1, max_it
         H = (I - q * np.outer(s, y)) @ H @ (I - q * np.outer(s, y)) + q * np.outer(s, s)
         x = new_x
 
-    return x
+    return x, iter
+
+
+def lbfgs(x=np.array([0, 0, 0]), epsilon=1e-1, max_iter=2000, mem_limit=10):
+    I = np.eye(3)
+    a = []
+    s = []
+    y = []
+    H = I
+    for iter in range(max_iter):
+        g = grad(x)
+        if np.linalg.norm(g) < epsilon:
+            break
+
+        q = g
+        for i in range(len(s) - 1, -1, -1):
+            a[i] = (1 / (y[i] @ s[i])) * s[i] @ q
+            q -= a[i] * y[i]
+
+        if iter != 0:
+            approx_y = (s[-1] @ y[-1]) / (y[-1] @ y[-1])
+            H = approx_y * I
+        z = H @ q
+
+        for i in range(len(s)):
+            B = (1 / (y[i] @ s[i])) * y[i] @ z
+            z = z + s[i] * (a[i] - B)
+
+        z = -z
+        new_x = x + z
+        if len(s) == mem_limit:
+            s.pop(0)
+            y.pop(0)
+            a.pop(0)
+
+        s.append(new_x - x)
+        y.append(grad(new_x) - g)
+        a.append(0)
+
+        x = new_x
+    return x, iter
 
 
 def linear_step_size_search(x, p, c1, c2, a_max, max_iter):
@@ -65,5 +105,11 @@ def zoom(x, p, a_low, a_high, c1, c2):
             return (a_low + a_high) / 2
 
 
-x = bfgs()
-print(f'Funtion min point = {x}; f(x) = {f(x)}')
+x, iter = bfgs()
+print(f'bfgs')
+print(f'Funtion min point = {x}; f(x) = {f(x)}. Finished in {iter} iterations.')
+
+x, iter = lbfgs()
+print(f'l-bfgs')
+print(f'Funtion min point = {x}; f(x) = {f(x)}. Finished in {iter} iterations.')
+
